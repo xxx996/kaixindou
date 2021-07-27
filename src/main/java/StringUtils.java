dd0dd9a11557594c93945e6b7d4ed706dc06bf60
zeze.li <lizeze.boy@gmail.com>
2021-07-27 20:47:59 +0800

    first commit

---
+++ src/main/java/StringUtils.java
@@ -0,0 +1,14 @@
+/**
+ * @BelongsProject: tool
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: lizeze
+ * @CreateTime: 2021-07-27 20:37
+ */
+public class StringUtils {
+
+
+    public static boolean isNull() {
+
+        return "".equals("");
+    }
+}
